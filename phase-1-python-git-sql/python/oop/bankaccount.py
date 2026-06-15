92d55e814521e5700eda9ed96ef5e28edc58baf4
Ayush28o <ayushraj280503@gmail.com>
2026-06-15 13:17:24 +0530

    day01: OOP - added transaction history and private balance

--- phase-1-python-git-sql/python/oop/bankaccount.py
+++ phase-1-python-git-sql/python/oop/bankaccount.py
@@ -1,25 +1,35 @@
-class bankaccount:
-    def __init__(self,owner,balance=0):
+class BankAccount:
+    def __init__(self, owner, balance=0, history=None):
         self.owner = owner
-        self.balance = balance
+        self.__balance  = balance
+        self.history = history if history is not None else []
 
-    def deposit(self,amount):
-        self.balance+=amount
-    
-    def withdraw (self,amount):
-        if self.balance<amount:
-         print ( "insufficient amount")
-        else:
-           self.balance-= amount;
+    def deposit(self, amount):
+        self.__balance  += amount
+        self.history.append(("deposit", amount))
 
+    def withdraw(self, amount):
+        if self.__balance  < amount:
+            print("insufficient amount")
+            return False
+        self.__balance  -= amount
+        self.history.append(("withdraw", amount))
+        return True
 
-        
-    def getbalance(self):
-       return self.balance 
+    def get_balance(self):
+        return self.__balance 
 
+acc1 = BankAccount("Nikhil", 5000)
+acc1.deposit(200)
+acc1.withdraw(500)
+print(acc1.get_balance())
 
-acc=bankaccount("Nikhil", 5000)
-acc.deposit (200)
-acc.withdraw (500)
-print(acc.getbalance())
+acc2 = BankAccount("Nikita", 1000)
+acc2.deposit(2000)
+acc2.withdraw(500)
+print(acc2.get_balance())
 
+acc3 = BankAccount("Ankit", 5000)
+acc3.deposit(2000)
+acc3.withdraw(5000)
+print(acc3.get_balance())
